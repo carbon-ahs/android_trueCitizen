16bac3c7d3c9cb0de2bc2d7a456af5672643777b
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-02-23 23:58:37 +0600

    qus comming from string res

--- app/src/main/java/com/axiagroups/truecitizen/MainActivity.java
+++ app/src/main/java/com/axiagroups/truecitizen/MainActivity.java
@@ -1,14 +1,33 @@
 package com.axiagroups.truecitizen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.axiagroups.truecitizen.databinding.ActivityMainBinding;
+import com.axiagroups.truecitizen.models.Question;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private int currentQusIndex = 0;
+    private Question[] questionBank = new Question[] {
+        //create/instantiate question objects
+        new Question(R.string.question_amendments, false), //correct: 27
+        new Question(R.string.question_constitution, true),
+        new Question(R.string.question_declaration, true),
+        new Question(R.string.question_independence_rights, true),
+        new Question(R.string.question_religion, true),
+        new Question(R.string.question_government, false),
+        new Question(R.string.question_government_feds, false),
+        new Question(R.string.question_government_senators, true),
+        //and add more!
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+        binding.questionTV.setText(questionBank[currentQusIndex].getAnsResId());
     }
 }
--- app/src/main/java/com/axiagroups/truecitizen/models/Question.java
+++ app/src/main/java/com/axiagroups/truecitizen/models/Question.java
@@ -1,2 +1,27 @@
-package com.axiagroups.truecitizen.models;public class Qustion {
+package com.axiagroups.truecitizen.models;
+
+public class Question {
+    private int ansResId;
+    private boolean ansTrue;
+
+    public Question(int ansResId, boolean ansTrue) {
+        this.ansResId = ansResId;
+        this.ansTrue = ansTrue;
+    }
+
+    public int getAnsResId() {
+        return ansResId;
+    }
+
+    public void setAnsResId(int ansResId) {
+        this.ansResId = ansResId;
+    }
+
+    public boolean isAnsTrue() {
+        return ansTrue;
+    }
+
+    public void setAnsTrue(boolean ansTrue) {
+        this.ansTrue = ansTrue;
+    }
 }
