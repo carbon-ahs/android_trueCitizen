072962bb29b0fbf01c4a8a01c75dbc9fc7c6781a
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-02-24 00:38:51 +0600

    btns works

--- app/src/main/java/com/axiagroups/truecitizen/MainActivity.java
+++ app/src/main/java/com/axiagroups/truecitizen/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.axiagroups.truecitizen.databinding.ActivityMainBinding;
 import com.axiagroups.truecitizen.models.Question;
@@ -28,6 +31,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+        //binding.questionTV.setText(questionBank[currentQusIndex].getAnsResId());
+        updateQuestion();
+        binding.nextBtn.setOnClickListener(view -> {
+            //Log.d("Main", "onCreate:" + currentQusIndex++);
+            currentQusIndex = (currentQusIndex + 1) % questionBank.length;
+            updateQuestion();
+        });
+
+        binding.prevBtn.setOnClickListener(view -> {
+            if (currentQusIndex > 0) {
+                currentQusIndex = (currentQusIndex - 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+        binding.trueBtn.setOnClickListener(view -> checkAnswer(true));
+        binding.falseBtn.setOnClickListener(view -> checkAnswer(false));
+    }
+
+    private void updateQuestion() {
         binding.questionTV.setText(questionBank[currentQusIndex].getAnsResId());
+    }
+
+    private void checkAnswer(boolean userChoice){
+        int messageId;
+        if (userChoice == questionBank[currentQusIndex].isAnsTrue()){
+            messageId = R.string.correct_answer;
+        }
+        else {
+            messageId = R.string.wrong_answer;
+        }
+        Toast.makeText(MainActivity.this, messageId, Toast.LENGTH_SHORT).show();
     }
 }
